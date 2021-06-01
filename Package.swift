f08fa7c1798bf02153c938614668b6066cafeb25
Juraj Ďurech <durech.juraj@gmail.com>
2021-06-01 12:08:19 +0200

    Deployment: Update release files to 0.3.4

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/wultra/cc7/releases/download/0.3.3/openssl-1.1.1h.xcframework.zip",
-            checksum: "1d4f781f19eee2dee63069e244b8d0ae9431d1354d55a8351043491c2e91a637")
+            url: "https://github.com/wultra/cc7/releases/download/0.3.4/openssl-1.1.1h.xcframework.zip",
+            checksum: "5d0fe6e2593421d937c57e988d0734041ecb44be7c6dc6db941836549ae27f50")
     ]
 )
